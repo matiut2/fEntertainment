faf8893e678fda819b6809d2974d365407b3d59b
Jqmey <jqmey@outlook.com>
2018-01-30 18:06:10 +0100

    Update playerInfo.js

--- commands/playerInfo.js
+++ commands/playerInfo.js
@@ -1,22 +1,23 @@
 'use strict';
 
-module.exports = async (obj) => {
-    const message = obj.message, utils = obj.utils, args = obj.args;
+module.exports = async (parameters) => {
+    const message = parameters.message;
+    const utils = parameters.utils;
+    const args = parameters.args;
 
     if(!args[0]) {
         message.reply('prawidłowe użycie: `!gracz <gracz>`!');
     } else {
-        utils.api().globalPlayer(args[0]).then(async (player) => {
+        utils.api.globalPlayer(args[0]).then(async (player) => {
             const embed = utils.embed('Informacje o graczu', 'property');
             embed.setThumbnail(`https://api.fcraft.pl/player/${args[0]}/head?size=16`);
             embed.addField('Gracz', utils.escapeMarkdown(player.nick), true);
             embed.addField('Konto', (player.premium.last ? 'Oryginalne' : 'Pirackie'), true);
             embed.addField('Pierwsze wejście', utils.datetime(player.time.first), true);
             embed.addField('Ostatnie wejście', utils.datetime(player.time.last), true);
-
             embed.addField('Aktywny', (utils.isActive(player.time.last) ? 'Tak' : 'Nie'), true);
 
-            const bans = await utils.api().banList();
+            const bans = await utils.api.banList();
 
             if(bans.find(ban => ban.uuid === player.uuid)) {
                 embed.addField('Zbanowany', 'Tak', true);
